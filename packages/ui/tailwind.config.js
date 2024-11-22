90c9b7273ca7077b5b9c9aeb1c9b2d8dce50e015
Chris Yang <yy4382@outlook.com>
2024-11-22 15:46:16 -0800

    feat: init ui

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+  	extend: {
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
