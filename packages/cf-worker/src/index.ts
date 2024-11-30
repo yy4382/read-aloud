708be4bf36d079361b5f17533e4f30c035e73c6f
Chris Yang <yy4382@outlook.com>
2024-11-29 17:43:22 -0800

    feat(cf-worker): serve index.html directly from src instead of fetching from external URL

--- packages/cf-worker/src/index.ts
+++ packages/cf-worker/src/index.ts
@@ -6,6 +6,13 @@ import api from "./api";
 const app = new OpenAPIHono();
 export default app;
 
+// @ts-expect-error
+import indexHtml from "./index.html";
+
+app.get("/", async (c) => {
+  return c.html(indexHtml);
+});
+
 app.route("/api", api);
 
 app.get("/robots.txt", (c) => c.text("User-agent: *\nDisallow: /"));
