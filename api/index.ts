7ae914987403fac416e52f29eca2e329a0d8a6bd
Chris Yang <yy4382@outlook.com>
2025-08-19 15:40:02 +0800

    .

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import app from "../src";
+
+declare global {
+  var DEBUG: boolean;
+}
+
+if (process.env.TOKEN)
+  console.info(`Running with token: ${globalThis.process.env.TOKEN}`);
+else
+  console.warn(
+    "Running without token, please consider set TOKEN in environment variables",
+  );
+
+export default app;
