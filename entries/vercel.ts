104b4eb72a9393d76371a38c71c35fe2251f811e
Chris Yang <yy4382@outlook.com>
2025-08-19 15:26:05 +0800

    .

---
+++ entries/vercel.ts
@@ -0,0 +1,15 @@
+import { handle } from "hono/vercel";
+import app from "../src";
+
+declare global {
+  var DEBUG: boolean;
+}
+
+if (process.env.TOKEN)
+  console.info(`Running with token: ${globalThis.process.env.TOKEN}`);
+else
+  console.warn(
+    "Running without token, please consider set TOKEN in environment variables",
+  );
+
+handle(app);
