64e02acb9448b63f4d4e2d521be22fd585d4db58
Chris Yang <yy4382@outlook.com>
2025-09-03 12:40:56 +0800

    feat: add graceful shutdown for server on SIGINT and SIGTERM signals

--- entries/node.ts
+++ entries/node.ts
@@ -11,4 +11,19 @@ else
 
 app.use("*", serveStatic({ root: "./public" }));
 
-serve(app);
+const server = serve(app);
+
+// graceful shutdown
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+});
